1f445db94245a27289372bd72647b22129f200bc
HassanAlnaggar99 <hassanalnaggar2024@gmail.com>
2023-11-16 19:11:49 +0200

    Updated UI and added a colorful button
    Updated the UI through font-family and
    background color and added a button to
    make a trailing random color functionality

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let n = 16;
 
-function drawPage(num) {
+function drawPage(num, colorful=false) {
   if (typeof num === "number"
       && num > 0
       && num <= 100) {
@@ -28,16 +28,27 @@ function drawPage(num) {
     let square = e.target;
     // console.log(e.target);
     if (square.getAttribute('class') === 'square') {
-      square.style.cssText = "background-color: black;";
+      if (colorful) {
+        let r = Math.floor(Math.random() * 256);
+        let g = Math.floor(Math.random() * 256);
+        let b = Math.floor(Math.random() * 256);
+        square.style.cssText = `background-color: rgb(${r},${g},${b});`;
+      } else {
+        square.style.cssText = "background-color: black;";
+      }
     }
   });
 }
 
-const btn_reset = document.querySelector("#reset");
-btn_reset.addEventListener('click', function(e) {
+function removeSketch() {
   const container = document.querySelector(".container");
   const body = document.querySelector("body");
   body.removeChild(container);
+}
+
+const btn_reset = document.querySelector("#reset");
+btn_reset.addEventListener('click', function(e) {
+  removeSketch();
   drawPage();
 });
 
@@ -45,10 +56,14 @@ const btn_new = document.querySelector("#newSize");
 btn_new.addEventListener('click', function(e) {
   let gridNumber = Number(prompt("Enter the side length of the grid: "));
 
-  const container = document.querySelector(".container");
-  const body = document.querySelector("body");
-  body.removeChild(container);
+  removeSketch();
   drawPage(gridNumber);
+});
+
+const btn_colorful = document.querySelector("#colorful");
+btn_colorful.addEventListener('click', function(e) {
+  removeSketch();
+  drawPage(-1, true);
 });
 
 drawPage();
