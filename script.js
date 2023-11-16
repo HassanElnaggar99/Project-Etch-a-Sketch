b49fdf2bc8cdc48957ab4698a65e6e523414d5fc
HassanAlnaggar99 <hassanalnaggar2024@gmail.com>
2023-11-16 18:48:49 +0200

    Added reset and new buttons
    Added reset and new buttons and
    finished their html, css and js.
    Finished the main functionality of this project.

--- script.js
+++ script.js
@@ -1,26 +1,54 @@
-const container = document.createElement("div");
-container.classList.add("container");
+let n = 16;
 
-let n = 4;
-
-for (let i = 0; i < n; i++) {
-  const row = document.createElement("div");
-  row.classList.add("row");
-  for (let j = 0; j < n; j++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    row.appendChild(square);
+function drawPage(num) {
+  if (typeof num === "number"
+      && num > 0
+      && num <= 100) {
+    n = num;
   }
-  container.appendChild(row);
+
+  const container = document.createElement("div");
+  container.classList.add("container");
+
+  for (let i = 0; i < n; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    for (let j = 0; j < n; j++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      row.appendChild(square);
+    }
+    container.appendChild(row);
+  }
+
+  const body = document.querySelector("body");
+  body.appendChild(container);
+
+  container.addEventListener('mouseover', function(e) {
+    let square = e.target;
+    // console.log(e.target);
+    if (square.getAttribute('class') === 'square') {
+      square.style.cssText = "background-color: black;";
+    }
+  });
 }
 
-const body = document.querySelector("body");
-body.appendChild(container);
-
-container.addEventListener('mouseover', function(e) {
-  let square = e.target;
-  // console.log(e.target);
-  if (square.getAttribute('class') === 'square') {
-    square.style.cssText = "background-color: black;";
-  }
+const btn_reset = document.querySelector("#reset");
+btn_reset.addEventListener('click', function(e) {
+  const container = document.querySelector(".container");
+  const body = document.querySelector("body");
+  body.removeChild(container);
+  drawPage();
 });
+
+const btn_new = document.querySelector("#newSize");
+btn_new.addEventListener('click', function(e) {
+  let gridNumber = Number(prompt("Enter the side length of the grid: "));
+
+  const container = document.querySelector(".container");
+  const body = document.querySelector("body");
+  body.removeChild(container);
+  drawPage(gridNumber);
+});
+
+drawPage();
