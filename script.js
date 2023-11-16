eeb66d96a7b3417ecabf1f745580e33670a96437
HassanAlnaggar99 <hassanalnaggar2024@gmail.com>
2023-11-16 15:01:09 +0200

    Made the body of the project with the html, css and js
    Made the grid with html and styled it with css
    then commented out the html and made it with javascript
    then I have added an event listener to make the hover effect
    By now I have finished the first 3 steps of the project

---
+++ script.js
@@ -0,0 +1,26 @@
+const container = document.createElement("div");
+container.classList.add("container");
+
+let n = 16;
+
+for (let i = 0; i < n; i++) {
+  const row = document.createElement("div");
+  row.classList.add("row");
+  for (let j = 0; j < n; j++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    row.appendChild(square);
+  }
+  container.appendChild(row);
+}
+
+const body = document.querySelector("body");
+body.appendChild(container);
+
+container.addEventListener('mouseover', function(e) {
+  let square = e.target;
+  // console.log(e.target);
+  if (square.getAttribute('class') === 'square') {
+    square.style.cssText = "background-color: black;";
+  }
+});
